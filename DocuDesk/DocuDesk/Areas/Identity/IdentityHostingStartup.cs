00943efca7baf47e4aa3a50903819f94f92b9ad8
Patil <pratik.patil@hp.com>
2021-04-23 02:28:28 +0530

    First Commit

---
+++ DocuDesk/DocuDesk/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DocuDesk.Areas.Identity.Data;
+using DocuDesk.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DocuDesk.Areas.Identity.IdentityHostingStartup))]
+namespace DocuDesk.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DocuDeskDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DocuDeskDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DocuDeskDBContext>();
+            });
+        }
+    }
+}
